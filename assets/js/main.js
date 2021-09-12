8946f2ce763c06fc7e293ce7ccce987b7ff451ed
salman <salmanshoaib840@gmail.com>
2021-09-12 19:42:06 +0500

    project setup

---
+++ assets/js/main.js
@@ -0,0 +1,511 @@
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow : ".pre-cat",
+    nextArrow : ".next-cat",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
+
+
+  $('.tabs').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow : ".pre-tab",
+    nextArrow : ".next-tab",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
+
+  $('.features').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow : ".pre-feat",
+    nextArrow : ".next-feat",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
+  $('.arrival').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow : ".pre-ari",
+    nextArrow : ".next-ari",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
+  $('.tabs-2').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow : ".pre-tab2",
+    nextArrow : ".next-tab2",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+
+// brand
+
+$('.photos-list').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-photo",
+  nextArrow : ".next-photo",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.all-pro').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-all",
+  nextArrow : ".next-all",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+// makeup new arrival
+$('.makeup').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-make",
+  nextArrow : ".next-make",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.skin-cares').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-skin",
+  nextArrow : ".next-skin",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.hair-cares').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-hair",
+  nextArrow : ".next-hair",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// pro-detail
+$('.rel-pro').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow : ".pre-re-pro",
+  nextArrow : ".next-re-pro",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.rel-articles').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow : ".pre-re-art",
+  nextArrow : ".next-re-art",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// header
+
+const header = document.querySelector("#header");
+window.addEventListener('scroll', function (e) {
+ 
+  if (window.scrollY > 80) {
+    header.classList.add('fixed-top')
+  } else {
+    header.classList.remove('fixed-top')
+  }
+})
+const mobileNav = document.querySelector('.mobile-menu');
+const bar = document.querySelector(".bar-1");
+const cancel = document.querySelector(".cancel")
+const body = document.querySelector("body")
+bar.addEventListener("click" , e => {
+  e.preventDefault()
+  mobileNav.classList.add("active")
+  cancel.classList.remove("active")
+  body.classList.add("active")
+  // if(header.classList.contains("fixed-top")){
+  //   header.classList.remove("fixed-top")
+  // }
+})
+cancel.addEventListener("click" , e => {
+  e.preventDefault()
+  mobileNav.classList.remove("active")
+  cancel.classList.add("active")
+  body.classList.remove("active")
+
+
+})
+const searchIcon = document.querySelector(".bar-2")
+const cancelSearch = document.querySelector(".cancel-icon")
+const searchWrap = document.querySelector(".wrap-for-search-modal")
+
+searchIcon.addEventListener("click" , e => {
+  e.preventDefault();
+  searchWrap.classList.add("active")
+  body.classList.add("active")
+
+  console.log("yes")
+})
+cancelSearch.addEventListener("click" , e => {
+  e.preventDefault();
+  searchWrap.classList.remove("active")
+  body.classList.remove("active")
+
+})
